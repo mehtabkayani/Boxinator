952d1b32cb2c6e41d21dc7b9f7cca6da792b1d58
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-09-14 16:46:02 +0200

    Added : Components: AdminMainPage, login, register, navbar, new shipments, user account, main page, Added route and style.css

---
+++ boxinator_frontend/src/components/admin/AdminMainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AdminMainPage = () => {
+    return (
+        <div>
+            <h1>Admin main page : </h1>
+        </div>
+    );
+}
+export default AdminMainPage;
