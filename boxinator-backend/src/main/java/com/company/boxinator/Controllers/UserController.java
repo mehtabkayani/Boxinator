5e5fe73e153aa0c37bfeb9e4c0c1875f709981af
Mehtab Kayani <mehtab.kayani@se.experis.com>
2020-09-15 13:32:56 +0200

    found the error /

--- boxinator-backend/src/main/java/com/company/boxinator/Controllers/UserController.java
+++ boxinator-backend/src/main/java/com/company/boxinator/Controllers/UserController.java
@@ -1,21 +1,36 @@
 package com.company.boxinator.Controllers;
 
 import com.company.boxinator.Models.User;
+import com.company.boxinator.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.List;
+
 @RestController
-@CrossOrigin()
+@RequestMapping("/api")
 public class UserController {
 
-
+@Autowired
+    UserRepository userRepository;
 
     @PostMapping("/register")
-    public String register(@RequestBody User user) {
+    public User register(@RequestBody User user) {
+      return userRepository.save(user);
 
-
-        return "Welcome!";
     }
 
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/hi")
+    public String hello(){
+        System.out.println("hello");
+        return "Hello";
+    }
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
