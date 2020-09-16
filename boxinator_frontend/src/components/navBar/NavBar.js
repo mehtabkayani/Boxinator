d7520618ab858d494053dcedb1fa01e5ed5cd6f6
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-09-16 15:51:14 +0200

    Changed the color of Boxinator in navbar

--- boxinator_frontend/src/components/navBar/NavBar.js
+++ boxinator_frontend/src/components/navBar/NavBar.js
@@ -7,18 +7,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const NavBar = () => {
     return (
         <div className="navContainer">
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand><Link to="/">Boxinator</Link></Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link> <Link to="/">Home</Link></Nav.Link>
-                <Nav.Link> <Link to="/mainPage">Main page</Link></Nav.Link>
-                <Nav.Link> <Link to="/userAccount">User account</Link></Nav.Link>
-                <Nav.Link> <Link to="/adminMainPage">admin page</Link></Nav.Link>
-            </Nav>
-            <Form inline>
-                <Link to="/login"><Button variant="outline-info">Login</Button></Link>
-            </Form>
-        </Navbar>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">Boxinator</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                    <Nav.Link><Link to="/mainPage">Main page</Link></Nav.Link>
+                    <Nav.Link><Link to="/userAccount">User account</Link></Nav.Link>
+                    <Nav.Link><Link to="/adminMainPage">admin page</Link></Nav.Link>
+                </Nav>
+                <Form inline>
+                    <Link to="/login"><Button variant="outline-info">Login</Button></Link>
+                </Form>
+            </Navbar>
         </div>
     );
 }
