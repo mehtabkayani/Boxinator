7167fb4c981eadbd50343e90e2af1921e0f1795e
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-09-24 16:55:26 +0200

    Fixed the navbar so that right user has the right access but it needs som fixes

--- boxinator_frontend/src/components/navBar/NavBar.js
+++ boxinator_frontend/src/components/navBar/NavBar.js
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/cjs/Navbar";
 import {Form, Button, Nav} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const NavBar = ({setAuth}) => {
+const NavBar = ({setAuth, isAuthenticated}) => {
     const logout = e => {
         e.preventDefault();
         try {
@@ -18,19 +18,48 @@ const NavBar = ({setAuth}) => {
     return (
         <div className="navContainer">
             <Navbar bg="dark" variant="dark">
+
                 <Navbar.Brand href="/">Boxinator</Navbar.Brand>
+                {/*Not done yet, just testing !! */}
+                {
+                    isAuthenticated === "ADMINISTRATOR" ?(
+                        <Fragment>
+                        <Nav className="mr-auto">
+                        <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                        <Nav.Link><Link to="/userAccount">User account</Link></Nav.Link>
+                        <Nav.Link><Link to="/adminMainPage">admin page</Link></Nav.Link>
+                    </Nav>
+                    <Form inline>
+                    <Link to="/login"><Button variant="outline-info">Login</Button></Link>
+                    </Form>
+                    <Form inline>
+                    <Button variant="outline-info" onClick={logout}>Logout</Button>
+                    </Form>
+                        </Fragment>
+                    ):isAuthenticated === "REGISTERED_USER" ?(
+                        <Fragment>
                             <Nav className="mr-auto">
                                 <Nav.Link><Link to="/">Home</Link></Nav.Link>
                                 <Nav.Link><Link to="/mainPage">Main page</Link></Nav.Link>
                                 <Nav.Link><Link to="/userAccount">User account</Link></Nav.Link>
-                                <Nav.Link><Link to="/adminMainPage">admin page</Link></Nav.Link>
+                            </Nav>
+
+                            <Form inline>
+                                <Button variant="outline-info" onClick={logout}>Logout</Button>
+                            </Form>
+                        </Fragment>
+                    ):(
+                        <Fragment>
+                            <Nav className="mr-auto">
+                                <Nav.Link><Link to="/">Home</Link></Nav.Link>
                             </Nav>
                             <Form inline>
                                 <Link to="/login"><Button variant="outline-info">Login</Button></Link>
                             </Form>
-                <Form inline>
-                <Button variant="outline-info" onClick={logout}>Logout</Button>
-                </Form>
+                        </Fragment>
+                    )
+
+                }
             </Navbar>
         </div>
     );
