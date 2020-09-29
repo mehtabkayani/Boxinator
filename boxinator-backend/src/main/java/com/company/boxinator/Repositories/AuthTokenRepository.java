39c28c5000c4df0df942d0ad03da80868e5c9352
Mehtab Kayani <mehtab.kayani@se.experis.com>
2020-09-29 13:09:14 +0200

    fixed user delete

--- boxinator-backend/src/main/java/com/company/boxinator/Repositories/AuthTokenRepository.java
+++ boxinator-backend/src/main/java/com/company/boxinator/Repositories/AuthTokenRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AuthTokenRepository extends JpaRepository<AuthToken,Integer> {
-    Optional<AuthToken> findByUserId(Integer userId);
+    AuthToken findByUserId(Integer userId);
+
 }
