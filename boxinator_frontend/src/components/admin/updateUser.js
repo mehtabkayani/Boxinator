05c6e71119a34f491bf5485cc3cafe67b7b6fd59
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2020-10-01 16:35:16 +0200

    fixed merge conflicts

--- boxinator_frontend/src/components/admin/updateUser.js
+++ boxinator_frontend/src/components/admin/updateUser.js
@@ -4,7 +4,7 @@ import {Button, Form} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import axios from "axios";
 import {useParams} from "react-router";
-import {DELETE} from '../../api/CRUD
+import {DELETE} from '../../api/CRUD';
 import AdminUpdateUserDialog from "../Dialog/AdminUpdateUserDialog";
 
 
